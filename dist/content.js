42e9d5f161237bc1397425560bf6a4c20855df7f
kei95 <kei0613shine.star@gmail.com>
2023-09-11 22:58:58 -0700

    Extract and couple skip and hide functions

--- dist/content.js
+++ dist/content.js
@@ -1,4 +1,5 @@
 "use strict";
+// =============== util functions ===============
 /** Util function to narrow down element to HTMLElement */
 function isHTMLElement(element) {
     return element instanceof HTMLElement;
@@ -11,56 +12,100 @@ function checkIsContainJapanese(inputString) {
     // Test the input string against the pattern
     return japaneseCharacterPattern.test(inputString);
 }
-function script(mutationsList, observer) {
+// =============== skip flags ===============
+// Returns boolean to tell the target tweet should skip the check.
+// If true, there's no need for executing logics to check beyond that
+function skipIfElementHidden(targetTweet) {
+    if (targetTweet.style["display"] === "none")
+        return true;
+    return false;
+}
+function skipIfHeaderElement(targetTweet) {
+    if (targetTweet.querySelector(`[role="heading"]`))
+        return true;
+    return false;
+}
+function skipIfTweetFromOwner(targetTweet, userId) {
+    const userNameNode = targetTweet.querySelector(`[data-testid="User-Name"]`);
+    const userNameWithId = userNameNode === null || userNameNode === void 0 ? void 0 : userNameNode.textContent;
+    if (userNameWithId === null || userNameWithId === void 0 ? void 0 : userNameWithId.includes(userId))
+        return true;
+    return false;
+}
+// =============== disable flags ===============
+// Returns boolean to tell the target tweet has been hidden by this function.
+// If so, there's no need for executing logics to check beyond that
+function hideNonTweetElement(targetTweet) {
+    // if the given element is just for "show replies" hide it
+    const isElementTweet = targetTweet.querySelector("article");
+    if (!isElementTweet) {
+        targetTweet.style.display = "none";
+        return true;
+    }
+    return false;
+}
+function hideVerifiedAccount(targetTweet) {
+    // if the user is verified, hide it
+    const isUserVerified = targetTweet.querySelector(`[data-testid="icon-verified"]`);
+    if (isUserVerified) {
+        targetTweet.style.display = "none";
+        return true;
+    }
+    return false;
+}
+function hideNonJapaneseAccount(targetTweet) {
+    const accountNameElement = targetTweet.querySelector(`[dir="ltr"]`);
+    const isAccountNameContainJapanese = checkIsContainJapanese(accountNameElement === null || accountNameElement === void 0 ? void 0 : accountNameElement.textContent);
+    if (!isAccountNameContainJapanese) {
+        targetTweet.style.display = "none";
+        return true;
+    }
+    return false;
+}
+function hideNonJapaneseTweet(targetTweet) {
     var _a;
+    // get text part of the given target tweet element
+    const tweetTextElement = targetTweet.querySelector(`[data-testid="tweetText"]`);
+    const isContainJapanese = checkIsContainJapanese((_a = tweetTextElement === null || tweetTextElement === void 0 ? void 0 : tweetTextElement.textContent) !== null && _a !== void 0 ? _a : undefined);
+    const isTweetTextContainCharacter = tweetTextElement === null || tweetTextElement === void 0 ? void 0 : tweetTextElement.querySelector("span");
+    if (!isTweetTextContainCharacter || !tweetTextElement || !isContainJapanese) {
+        targetTweet.style.display = "none";
+        return true;
+    }
+    return false;
+}
+function script(mutationsList, observer) {
     const isAtRepliesPage = window.location.pathname.includes("status");
     if (!isAtRepliesPage) {
         return;
     }
     const userId = window.location.pathname.split("/")[1];
-    // Function to be executed when the target element appears
+    // When mutation happens, iterate through tweets
     for (const mutation of mutationsList) {
-        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-            const targetTweets = document.querySelectorAll('[data-testid="cellInnerDiv"]');
-            for (const targetTweet of targetTweets) {
-                // if the element isn't HTMLElement, skip it
-                if (!isHTMLElement(targetTweet))
-                    continue;
-                // if the element is hidden already, skip it
-                if (targetTweet.style["display"] === "none")
-                    continue;
-                // if it's tweet owner, don't do anything
-                const userNameNode = targetTweet.querySelector(`[data-testid="User-Name"]`);
-                const userNameWithId = userNameNode === null || userNameNode === void 0 ? void 0 : userNameNode.textContent;
-                if (userNameWithId === null || userNameWithId === void 0 ? void 0 : userNameWithId.includes(userId))
-                    continue;
-                // if the user is verified, hide it
-                const isUserVerified = targetTweet.querySelector(`[data-testid="icon-verified"]`);
-                if (isUserVerified) {
-                    targetTweet.style.display = "none";
-                    continue;
-                }
-                // if the node is just for "show replies" hide it
-                const isElementTweet = targetTweet.querySelector("article");
-                if (!isElementTweet) {
-                    targetTweet.style.display = "none";
-                    continue;
-                }
-                // if tweet only contains emoji, hide it
-                const textElement = targetTweet.querySelector(`[data-testid="tweetText"]`);
-                // if tweet doesn't contain any Japanese character, hide it
-                const isContainJapanese = checkIsContainJapanese((_a = textElement === null || textElement === void 0 ? void 0 : textElement.textContent) !== null && _a !== void 0 ? _a : undefined);
-                if (!(textElement === null || textElement === void 0 ? void 0 : textElement.querySelector("span")) ||
-                    !textElement ||
-                    !isContainJapanese) {
-                    targetTweet.style.display = "none";
-                    continue;
-                }
-            }
+        if (mutation.type !== "childList" || mutation.addedNodes.length === 0)
+            return;
+        const targetTweets = document.querySelectorAll('[data-testid="cellInnerDiv"]');
+        for (const targetTweet of targetTweets) {
+            // skip checks - if any of the following is true, skip the check
+            if (!isHTMLElement(targetTweet))
+                continue;
+            if (skipIfElementHidden(targetTweet))
+                continue;
+            if (skipIfHeaderElement(targetTweet))
+                continue;
+            if (skipIfTweetFromOwner(targetTweet, userId))
+                continue;
+            // hide checks - if any of the following is true, hide the tweet
+            if (hideNonJapaneseAccount(targetTweet))
+                continue;
+            if (hideNonTweetElement(targetTweet))
+                continue;
+            if (hideNonJapaneseTweet(targetTweet))
+                continue;
         }
     }
 }
-// Create a MutationObserver with the elementAppearedCallback function
+// Create a MutationObserver to trigger the script when DOM updates
 const observer = new MutationObserver(script);
 // Specify the target node and the type of mutations to observe
 const targetNode = document.body;
--- scripts/content.ts
+++ scripts/content.ts
@@ -1,9 +1,10 @@
+// =============== util functions ===============
 /** Util function to narrow down element to HTMLElement */
 function isHTMLElement(element: Element): element is HTMLElement {
   return element instanceof HTMLElement;
 }
 
-function checkIsContainJapanese(inputString?: string) {
+function checkIsContainJapanese(inputString?: string | null) {
   if (!inputString) return false;
 
   // Define a regular expression pattern for Japanese characters
@@ -14,6 +15,92 @@ function checkIsContainJapanese(inputString?: string) {
   return japaneseCharacterPattern.test(inputString);
 }
 
+// =============== skip flags ===============
+// Returns boolean to tell the target tweet should skip the check.
+// If true, there's no need for executing logics to check beyond that
+
+function skipIfElementHidden(targetTweet: HTMLElement): boolean {
+  if (targetTweet.style["display"] === "none") return true;
+  return false;
+}
+
+function skipIfHeaderElement(targetTweet: HTMLElement): boolean {
+  if (targetTweet.querySelector(`[role="heading"]`)) return true;
+  return false;
+}
+
+function skipIfTweetFromOwner(
+  targetTweet: HTMLElement,
+  userId: string
+): boolean {
+  const userNameNode = targetTweet.querySelector(`[data-testid="User-Name"]`);
+  const userNameWithId = userNameNode?.textContent;
+  if (userNameWithId?.includes(userId)) return true;
+
+  return false;
+}
+
+// =============== disable flags ===============
+// Returns boolean to tell the target tweet has been hidden by this function.
+// If so, there's no need for executing logics to check beyond that
+
+function hideNonTweetElement(targetTweet: HTMLElement): boolean {
+  // if the given element is just for "show replies" hide it
+  const isElementTweet = targetTweet.querySelector("article");
+  if (!isElementTweet) {
+    targetTweet.style.display = "none";
+    return true;
+  }
+
+  return false;
+}
+
+function hideVerifiedAccount(targetTweet: HTMLElement): boolean {
+  // if the user is verified, hide it
+  const isUserVerified = targetTweet.querySelector(
+    `[data-testid="icon-verified"]`
+  );
+
+  if (isUserVerified) {
+    targetTweet.style.display = "none";
+    return true;
+  }
+
+  return false;
+}
+
+function hideNonJapaneseAccount(targetTweet: HTMLElement): boolean {
+  const accountNameElement = targetTweet.querySelector(`[dir="ltr"]`);
+  const isAccountNameContainJapanese = checkIsContainJapanese(
+    accountNameElement?.textContent
+  );
+
+  if (!isAccountNameContainJapanese) {
+    targetTweet.style.display = "none";
+    return true;
+  }
+
+  return false;
+}
+
+function hideNonJapaneseTweet(targetTweet: HTMLElement): boolean {
+  // get text part of the given target tweet element
+  const tweetTextElement = targetTweet.querySelector(
+    `[data-testid="tweetText"]`
+  );
+  const isContainJapanese = checkIsContainJapanese(
+    tweetTextElement?.textContent ?? undefined
+  );
+  const isTweetTextContainCharacter = tweetTextElement?.querySelector("span");
+
+  if (!isTweetTextContainCharacter || !tweetTextElement || !isContainJapanese) {
+    targetTweet.style.display = "none";
+    return true;
+  }
+
+  return false;
+}
+
 function script(mutationsList: MutationRecord[], observer: MutationObserver) {
   const isAtRepliesPage = window.location.pathname.includes("status");
   if (!isAtRepliesPage) {
@@ -22,64 +109,30 @@ function script(mutationsList: MutationRecord[], observer: MutationObserver) {
 
   const userId = window.location.pathname.split("/")[1];
 
-  // Function to be executed when the target element appears
+  // When mutation happens, iterate through tweets
   for (const mutation of mutationsList) {
-    if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-      const targetTweets = document.querySelectorAll(
-        '[data-testid="cellInnerDiv"]'
-      );
+    if (mutation.type !== "childList" || mutation.addedNodes.length === 0)
+      return;
+    const targetTweets = document.querySelectorAll(
+      '[data-testid="cellInnerDiv"]'
+    );
 
-      for (const targetTweet of targetTweets) {
-        // if the element isn't HTMLElement, skip it
-        if (!isHTMLElement(targetTweet)) continue;
-        // if the element is hidden already, skip it
-        if (targetTweet.style["display"] === "none") continue;
+    for (const targetTweet of targetTweets) {
+      // skip checks - if any of the following is true, skip the check
+      if (!isHTMLElement(targetTweet)) continue;
+      if (skipIfElementHidden(targetTweet)) continue;
+      if (skipIfHeaderElement(targetTweet)) continue;
+      if (skipIfTweetFromOwner(targetTweet, userId)) continue;
 
-        // if it's tweet owner, don't do anything
-        const userNameNode = targetTweet.querySelector(
-          `[data-testid="User-Name"]`
-        );
-        const userNameWithId = userNameNode?.textContent;
-        if (userNameWithId?.includes(userId)) continue;
-
-        // if the user is verified, hide it
-        const isUserVerified = targetTweet.querySelector(
-          `[data-testid="icon-verified"]`
-        );
-        if (isUserVerified) {
-          targetTweet.style.display = "none";
-          continue;
-        }
-
-        // if the node is just for "show replies" hide it
-        const isElementTweet = targetTweet.querySelector("article");
-        if (!isElementTweet) {
-          targetTweet.style.display = "none";
-          continue;
-        }
-
-        // if tweet only contains emoji, hide it
-        const textElement = targetTweet.querySelector(
-          `[data-testid="tweetText"]`
-        );
-        // if tweet doesn't contain any Japanese character, hide it
-        const isContainJapanese = checkIsContainJapanese(
-          textElement?.textContent ?? undefined
-        );
-        if (
-          !textElement?.querySelector("span") ||
-          !textElement ||
-          !isContainJapanese
-        ) {
-          targetTweet.style.display = "none";
-          continue;
-        }
-      }
+      // hide checks - if any of the following is true, hide the tweet
+      if (hideNonJapaneseAccount(targetTweet)) continue;
+      if (hideNonTweetElement(targetTweet)) continue;
+      if (hideNonJapaneseTweet(targetTweet)) continue;
     }
   }
 }
 
-// Create a MutationObserver with the elementAppearedCallback function
+// Create a MutationObserver to trigger the script when DOM updates
 const observer = new MutationObserver(script);
 
 // Specify the target node and the type of mutations to observe
