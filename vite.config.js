0c6d1d4fe6195d199167abb3085ba47ce9f21666
kei95 <kei0613shine.star@gmail.com>
2023-09-23 20:27:13 -0700

    Integrate Vitest with jsdom env

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import { crx } from "@crxjs/vite-plugin";
 import manifest from "./manifest.json";
 
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
   plugins: [crx({ manifest })],
 });
