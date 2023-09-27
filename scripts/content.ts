509109f9d71616a54d5feac85a1de3667dcc6216
kei95 <kei0613shine.star@gmail.com>
2023-09-26 18:14:49 -0700

    Add flag to stop false positives on non-japanese thread

--- scripts/content.ts
+++ scripts/content.ts
@@ -6,13 +6,17 @@ import {
 import {
   checkIfHTMLElement,
   skipIfTweetIsHidden,
-  skipIfTweetFromOwner,
+  skipIfTweetFromOwner as skipIfOriginalTweet,
+  skipIfOriginalTweetIsNonJapanese,
 } from "./skipFlags";
 
 // =============== util functions ===============
 function hideTweet(targetTWeet: HTMLElement) {
   targetTWeet.style.display = "none";
 }
+
+/** Flag to tell if the original tweet of current thread is non japanese */
+let nonJapaneseThreadFlag = false;
 
 /** Script to run when observer catches DOM updates */
 function script() {
@@ -30,14 +34,21 @@ function script() {
   );
 
   // if targetTweets don't exist in DOM, skip the execution
-  if (!targetTweets) return;
+  if (!targetTweets || !baseElement) return;
 
   // Iterate though tweets and apply style update to hide ones that are "inappropriate"
   for (const targetTweet of targetTweets) {
     // skip checks - if any of the following is true, skip the check
     if (!checkIfHTMLElement(targetTweet)) continue;
     if (skipIfTweetIsHidden(targetTweet)) continue;
-    if (skipIfTweetFromOwner(targetTweet)) continue;
+    if (skipIfOriginalTweet(targetTweet)) {
+      // When the original tweet is being checked, update the flag to tell whether if it's Japanese
+      nonJapaneseThreadFlag = skipIfOriginalTweetIsNonJapanese(targetTweet);
+      continue;
+    }
+
+    // If the original tweet is not in Japanese, skip the check
+    if (nonJapaneseThreadFlag) continue;
 
     // hide checks - if any of the following is true, hide the tweet
     if (hideNonTweetElement(targetTweet)) {
