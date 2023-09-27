3d71585dbe8c4a7869bb0145b9e4361432206fc1
kei95 <kei0613shine.star@gmail.com>
2023-09-26 18:19:20 -0700

    bump up and generate build for next release

--- dist/assets/content-script-loader.content.ts-8a024744-2bd48517.js
+++ dist/assets/content-script-loader.content.ts-8a024744-2bd48517.js
@@ -4,7 +4,7 @@
   (async () => {
     await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/content.ts-5bd8e6b9.js")
+      chrome.runtime.getURL("assets/content.ts-8a024744.js")
     );
   })().catch(console.error);
 
